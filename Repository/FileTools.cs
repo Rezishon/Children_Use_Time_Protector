219179c6509e47b7e403313d99bc71af68f6fc16
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-11 18:49:36 +0330

    feat: add repository for file working
    This class will use as a repository for file working. In this app we will use a file as our local database

---
+++ Repository/FileTools.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Children_Use_Time_Protector.Repository
+{
+    public class FileTools
+    {
+       public FileTools(Parameters)
+       {
+        
+       } 
+    }
+}
