69f64d1cdb23c93d981049801ae9d5530ae47c9d
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-11 23:51:52 +0330

    feat: add  dependencies
    We need io class for dealing with files, text for encoding and cryptography for Hashing

--- Repository/FileTools.cs
+++ Repository/FileTools.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Children_Use_Time_Protector.Repository
