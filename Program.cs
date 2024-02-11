5ba9200ba5f14f4932cbeca97201f52e4abb07f9
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-11 23:48:44 +0330

    feat: add dependencies
    We need text class for encoding and cryptography for hash which we use sha256 in our app

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿namespace Children_Use_Time_Protector;
+﻿using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+using System.Text;
+using Children_Use_Time_Protector.Repository;
+
+namespace Children_Use_Time_Protector;
 
 class Program
 {
--- Repository/FileTools.cs
+++ Repository/FileTools.cs
@@ -8,5 +8,22 @@ namespace Children_Use_Time_Protector.Repository
     public class FileTools(string Path = "./DataBase.cutp")
     {
         private readonly string path = Path;
+
+        public async Task<bool> ReadAppStatus()
+        {
+            Exist_Of_Database_File();
+
+            string[] linesOfText = await File.ReadAllLinesAsync(path);
+            return int.Parse(linesOfText[0].Trim()) == 0;
+        }
+
+        public async void Exist_Of_Database_File()
+        {
+            if (!File.Exists(path))
+            {
+                await File.WriteAllTextAsync(path, "0");
+                // ask for root password
+            }
+        }
     }
 }
