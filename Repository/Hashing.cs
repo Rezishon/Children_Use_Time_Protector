24c77e3a668dc7196a0e403210912a8ec9070c14
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-12 00:07:29 +0330

    feat: converter to sha256
    Because C# doesn't have easy way to convert strings to sha256 so we will need a function for it. This function will use as an extension method. Hashing in C# has some parts: 1.create a instance of hashing algorithm; 2. Encode with a algorithm; 3. Do the main hash; 4. Convert the result to string

--- Repository/Hashing.cs
+++ Repository/Hashing.cs
@@ -10,6 +10,12 @@ namespace Children_Use_Time_Protector.Repository
 {
     public class Hashing
     {
-        
+        public static string ToSha256(string messageString)
+        {
+            using var mysha256 = SHA256.Create();
+            byte[] messageBytes = Encoding.UTF8.GetBytes(messageString);
+            byte[] hashValue = SHA256.HashData(messageBytes);
+            return BitConverter.ToString(hashValue).Replace("-", string.Empty);
+        }
     }
 }
