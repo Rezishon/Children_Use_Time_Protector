dc9e58fafb0d900a440b4e036956630491e1204a
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-12 00:13:27 +0330

    feat: add class for messages
    Because we may want to change strings in future like the app name or the showing messages of start up i prefer to add them to a class so if i use them in multi pages i won't miss them

---
+++ Repository/Messages.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Children_Use_Time_Protector.Repository
+{
+    public class Messages
+    {
+        
+    }
+}
