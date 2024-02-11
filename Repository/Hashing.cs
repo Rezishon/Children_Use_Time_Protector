87f99000e8f1418e0a36346c5effdf0c04bfa7bd
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-12 00:10:04 +0330

    feat: add compare function for hash
    We need to compare two hash some times so i add this method which will get two string which will be the hash codes in string type. Then returns true or false it the strings are exactly equals

--- Repository/Hashing.cs
+++ Repository/Hashing.cs
@@ -17,5 +17,10 @@ namespace Children_Use_Time_Protector.Repository
             byte[] hashValue = SHA256.HashData(messageBytes);
             return BitConverter.ToString(hashValue).Replace("-", string.Empty);
         }
+
+        public static bool CompareSha256s(string strF, string strS)
+        {
+            return string.Equals(strF, strS);
+        }
     }
 }
