ec39cc97300e6574b35721b8b77b82a2510ebbdb
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-12 00:00:10 +0330

    feat: add class for hashing
    This class will contain functions which related to hashing

---
+++ Repository/Hashing.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Children_Use_Time_Protector.Repository
+{
+    
+}
