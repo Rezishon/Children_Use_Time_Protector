169846c59f681875dd3b7d84b2db3ae17bc79869
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-12 23:29:52 +0330

    feat: manage constructor
    We will use the Timer class with 1000 millisecond which is equal to 1 second. Also we should tell her that we want you to repeat after the times elapsed so we need to set AutoReset to true

--- Services/CUTP.cs
+++ Services/CUTP.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Timer = System.Timers.Timer;
+
 namespace Services
 {
     public class CUTP
     {
         private readonly Timer _timer;
+
+        public CUTP()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+        }
     }
 }
