622f7a7e123c7fe43d9277a29c2edd09f3f953ae
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-14 16:46:11 +0330

    feat: set log content
    I use a property with get method for storing my logs content

--- Services/Repository/LogHandling.cs
+++ Services/Repository/LogHandling.cs
@@ -3,5 +3,6 @@ namespace LogHandling
     public static class LogFile
     {
         public static string LogFilePath { get; } = "./Log.cutp";
+        public static string LogContent { get; } = DateTime.Now.ToString();
     }
 }
