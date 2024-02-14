6ec6d8fbbce76a1593298f453fe16543b04e91c3
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-14 15:56:40 +0330

    feat: set Service Part
    Here I added the raw service config string to ServicePart variable, also I use the NullString variable which declared upper as a string variable

--- Repository/FileTools.cs
+++ Repository/FileTools.cs
@@ -22,6 +22,8 @@ namespace ConfigHandling
         {
                 string RootPart =
                     $"{Hashing.Hash.ToSha256("0")};{NullString};{NullString};{NullString};{NullString};";
+                string ServicePart =
+                    $"{Hashing.Hash.ToSha256("0")};{NullString};{NullString};{NullString};{NullString};{NullString};{NullString};{NullString};{NullString};";
         }
     }
         public async Task<bool> ReadAppStatus()
