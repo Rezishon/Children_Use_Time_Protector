2c52b869c6fe555de5ac22725c90db0822d779dc
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-15 00:09:50 +0330

    feat: dictionary for commands
    All the commands I will use in future stores here with a name. The overview of this dictionary is that there's commands name as key and the commands themselves are as value

--- Repository/CommandHandling.cs
+++ Repository/CommandHandling.cs
@@ -4,5 +4,11 @@ namespace CommandHandling
 {
     public static class Commands
     {
+        public static Dictionary<string, string> CommandsDic = new Dictionary<string, string>()
+        {
+            { "Shutdown", "shutdown.exe /s /f" },
+            { "TurnOnService", @"..\Services\bin\Debug\net8.0\Services.exe install start" },
+            { "TurnOffService", @"..\Services\bin\Debug\net8.0\Services.exe uninstall" }
+        };
     }
 }
