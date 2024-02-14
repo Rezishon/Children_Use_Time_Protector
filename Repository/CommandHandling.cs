f4fe55795d24e98c858fd68057e6361ebcd97da3
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-15 00:38:49 +0330

    fix: add try catch architecture
    Also will return the error message in console

--- Repository/CommandHandling.cs
+++ Repository/CommandHandling.cs
@@ -13,6 +13,8 @@ namespace CommandHandling
 
         private static void CommandRunner(string CommandNameString)
         {
+            try
+            {
                 var processInfo = new ProcessStartInfo
                 {
                     Verb = "runas", // Run as administrator
@@ -25,6 +27,11 @@ namespace CommandHandling
                 };
 
                 _ = Process.Start(processInfo);
+            }
+            catch (System.Exception e)
+            {
+                System.Console.WriteLine(e);
+            }
         }
     }
 }
