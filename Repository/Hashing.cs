fffa8d7c1f95a89f9c1af6d5e265455ff7beb9b5
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-14 16:12:06 +0330

    fix: rename parameter
    The parameter name could make misunderstood

--- Repository/Hashing.cs
+++ Repository/Hashing.cs
@@ -10,10 +10,10 @@ namespace Hashing
 {
     public static class Hash
     {
-        public static string ToSha256(string messageString)
+        public static string ToSha256(string String)
         {
             using var mysha256 = SHA256.Create();
-            byte[] messageBytes = Encoding.UTF8.GetBytes(messageString);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(String);
             byte[] hashValue = SHA256.HashData(messageBytes);
             return BitConverter.ToString(hashValue).Replace("-", string.Empty);
         }
