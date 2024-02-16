970877c0e9d24178aabd5a60fe9c62eee2b8cc12
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-16 23:00:04 +0330

    feat: return app name
    This method will return the app name.

--- Manage/Repository/Messages.cs
+++ Manage/Repository/Messages.cs
@@ -4,5 +4,9 @@ namespace Children_Use_Time_Protector.Repository
 {
     public class Messages
     {
+        public static string Say_App_Name()
+        {
+            return "Children Use Time Protector";
+        }
     }
 }
