9cf9338dc1536a64e4aa460fd2623d8f3d722e71
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-16 21:19:49 +0330

    feat: dictionary for commands
    In this dictionary each command has a name and the actual prompt of powershell

--- Manage/Repository/CommandHandling.cs
+++ Manage/Repository/CommandHandling.cs
@@ -4,5 +4,12 @@ namespace CommandHandling
 {
     public static class Commands
     {
+        public static Dictionary<string, string> CommandsDic = new Dictionary<string, string>()
+        {
+            { "Shutdown", "shutdown.exe /s /f" },
+            // ..\..\..\..\..\Services\bin\Debug\net8.0\Services.exe
+            { "TurnOnService", @"rojects\Children_Use_Time_Protector\Services\bin\Debug\net8.0\Services.exe install start" },
+            { "TurnOffService", @"..\Services\bin\Debug\net8.0\Services.exe uninstall" }
+        };
     }
 }
