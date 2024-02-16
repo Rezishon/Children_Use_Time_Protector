ae394cf5e39c8de4c09c31f01d144c5eee259770
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-16 20:52:01 +0330

    chore [Retry]: additional files
    Because the designing of this project was wrong I should start it from start but i chose to keep some part of code and recommit other parts. It's like nightmare but this is the way of learning. It's my first project in dotnet core and vscode so it's kind of normal :smile:

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0241fa3f21e0b12bec45e657719fe99e523ecdd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7508a79cc67c3680450f636a81b10f27a4cf7e4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
