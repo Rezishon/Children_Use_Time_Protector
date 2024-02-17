6d3b7c1cc9f07fecc9fde524131ab2e64fc4159a
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-18 00:05:25 +0330

    fix: return same value
    This property changed because we was using static type according to class type, so that made the property to get data only once which made un in trouble. So I replaced it directly in caller function

--- Services/Repository/LogHandling.cs
+++ Services/Repository/LogHandling.cs
@@ -3,6 +3,5 @@ namespace LogHandling
     public static class LogFile
     {
         public static string LogFilePath { get; } = "./Log.cutp";
-        public static string LogContent { get; } = DateTime.Now.ToString();
     }
 }
