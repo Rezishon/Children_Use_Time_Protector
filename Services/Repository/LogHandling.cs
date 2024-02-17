a2970b0a1ac8179265e69270c70835fcb8345edf
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-18 00:19:30 +0330

    feat: return log value
    This method will read the whole log file and returns them line by line

--- Services/Repository/LogHandling.cs
+++ Services/Repository/LogHandling.cs
@@ -3,5 +3,10 @@ namespace LogHandling
     public static class LogFile
     {
         public static string LogFilePath { get; } = "./Log.cutp";
+
+        public static string[] LogReader()
+        {
+            return File.ReadAllLines(LogFilePath);
+        }
     }
 }
