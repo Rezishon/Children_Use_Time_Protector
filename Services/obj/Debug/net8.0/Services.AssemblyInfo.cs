1dc7aec318a1e2431b3f2f83e4cf459ba7e75760
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-26 01:44:01 +0330

    feat: additional files

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b01f0003abe5a02df14b61877b91b7d15d4e69ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f405c6d13d6aa9cba4a3cb9d007ab0b057a5c759")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
