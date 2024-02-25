6ee525da1bf81e012486710dec62740b9824726f
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-25 22:47:29 +0330

    fix: add dependencies
    The namespace also replaced because of add these dependencies

--- Manage/Program.cs
+++ Manage/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace Children_Use_Time_Protector;
+﻿using CommandHandling;
+using ConfigHandling;
+using Hashing;
+using Spectre.Console;
+
+namespace Children_Use_Time_Protector;
 
 class Program
 {
