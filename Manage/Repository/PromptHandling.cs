f1a9b6015026e04794429dcfc738cca5fa84e9da
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-27 23:01:54 +0330

    feat: create loop
    Because the taking password should be complete with some factors, this process should be inside of a loop and after the process has complete the loop ends.

--- Manage/Repository/PromptHandling.cs
+++ Manage/Repository/PromptHandling.cs
@@ -29,6 +29,11 @@ namespace Manage.Repository
             bool NeedsHint = false
         )
         {
+            bool flag = true;
+
+            while (flag)
+            {
+            }
         }
     }
 }
