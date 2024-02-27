eac44f63ddd8f0e923477a31615af2baa3fa4b7a
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-27 22:47:48 +0330

    feat [Menu]: add menu prompt
    This method is inside of Spectre.Console package. It has a title, page size, style of selected line

--- Manage/Repository/PromptHandling.cs
+++ Manage/Repository/PromptHandling.cs
@@ -10,6 +10,14 @@ namespace Manage.Repository
             Dictionary<string, int> PartsDictionary = new Dictionary<string, int>();
 
             items.ForEach(x => PartsDictionary.Add(x, items.IndexOf(x)));
+
+            string chosenPartMenu = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("What part you want to change?")
+                    .PageSize(items.Count)
+                    .HighlightStyle(Style.Parse("purple bold"))
+                    .AddChoices(items)
+            );
         }
     }
 }
