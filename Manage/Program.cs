dbcd8729da7e573a560b777eda5371f6a705091b
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-28 17:14:24 +0330

    feat: add cases
    For each line of root menu we should have a case

--- Manage/Program.cs
+++ Manage/Program.cs
@@ -67,7 +67,18 @@ class Program
                         )[1]
                     )
                     {
+                        case 0:
+                            break;
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
                     }
+                    break;
                 case 2:
                     break;
             }
