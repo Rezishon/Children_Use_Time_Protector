f5bbd7be5ab1e56678697f12ece028b5ec75f590
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-03-01 00:38:49 +0330

    chore: additional files

--- Manage/obj/Debug/net8.0/Children_Use_Time_Protector.AssemblyInfo.cs
+++ Manage/obj/Debug/net8.0/Children_Use_Time_Protector.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Children_Use_Time_Protector")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb638280c276c021d6c13cbee4effbeffe6b1fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eee54ac5bea7ea85ed7327dea112d544050d98fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Children_Use_Time_Protector")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Children_Use_Time_Protector")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
