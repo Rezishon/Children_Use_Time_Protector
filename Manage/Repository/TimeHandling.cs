3dd15391b9cd7d4442dca02b53048b12364555fb
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-03-02 23:51:26 +0330

    feat: add try catch structure
    It prints error message to console

--- Manage/Repository/TimeHandling.cs
+++ Manage/Repository/TimeHandling.cs
@@ -6,10 +6,18 @@ namespace TimeHandling
         {
             int UsedDurationTime = 0;
             int AllowedDurationTime = 0;
+
+            try
+            {
                 UsedDurationTime = LogHandling.LogFile.LogReader().Length * 10;
                 AllowedDurationTime =
                     ConfigHandling.ServicePart.AllowedDuration()
                     + ConfigHandling.ServicePart.TempAllowedDuration();
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
         }
     }
 }
