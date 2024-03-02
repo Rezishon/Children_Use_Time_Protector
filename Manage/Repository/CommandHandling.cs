afae15bc1757908cce2bbf2e859d5d1c6261101a
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-03-03 00:36:45 +0330

    fix: forgotten curly braket

--- Manage/Repository/CommandHandling.cs
+++ Manage/Repository/CommandHandling.cs
@@ -38,6 +38,7 @@ namespace CommandHandling
                     {
                         return output;
                     }
+                }
             }
             catch (Exception e)
             {
