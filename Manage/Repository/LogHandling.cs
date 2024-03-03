a8201fd9260c85b21c4768921773d91e63fb0a27
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-03-03 22:17:52 +0330

    feat: log file path
    When this property get use because of it's static, it will set for other part of codes too. beside it's read only, it runs the pwd command to found where it is in user device, then does some normaliztion on it which are repalce some names and etc. This normaliztion does with regex lib

--- Manage/Repository/LogHandling.cs
+++ Manage/Repository/LogHandling.cs
@@ -5,5 +5,11 @@ namespace LogHandling
 {
     public static class LogFile
     {
+        public static string LogFilePath { get; } =
+            Regex.Replace(
+                Commands.Pwd(),
+                @"\\Children_Use_Time_Protector\\(\w*\W*)*$",
+                @"\Children_Use_Time_Protector\Manage\bin\Debug\net8.0\Log.cutp"
+            );
     }
 }
