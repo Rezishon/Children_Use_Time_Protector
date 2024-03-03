0edbb2fadd01c5871ecad23cffde21d48ea36dc3
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-03-03 22:19:34 +0330

    feat: create log reader method
    This method reads log file contents in format of array of string in given path and then returns an array of string which are file content line by line. It's static for easier access

--- Manage/Repository/LogHandling.cs
+++ Manage/Repository/LogHandling.cs
@@ -11,5 +11,10 @@ namespace LogHandling
                 @"\\Children_Use_Time_Protector\\(\w*\W*)*$",
                 @"\Children_Use_Time_Protector\Manage\bin\Debug\net8.0\Log.cutp"
             );
+
+        public static string[] LogReader()
+        {
+            return File.ReadAllLines(LogFilePath);
+        }
     }
 }
