666bf82ba87f65aef3b85ea52db1137a4423d162
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-03-05 20:14:06 +0330

    Update LogHandling.cs

--- Manage/Repository/LogHandling.cs
+++ Manage/Repository/LogHandling.cs
@@ -3,8 +3,9 @@ using CommandHandling;
 
 namespace LogHandling
 {
-    public static class LogFile
+    public class LogFile
     {
+        // Log file Path property
         public static string LogFilePath { get; } =
             Regex.Replace(
                 Commands.Pwd(),
@@ -12,29 +13,42 @@ namespace LogHandling
                 @"\Children_Use_Time_Protector\Manage\bin\Debug\net8.0\Log.cutp"
             );
 
+        // Read log file content
         public static string[] LogReader()
         {
             return File.ReadAllLines(LogFilePath);
         }
 
+        // Clear file content
         public static void LogCleaner()
         {
             File.WriteAllText(LogFilePath, "");
         }
 
-        public static string Date(string inputDate = "")
+        // Return date part of string
+        public static string Date(
+            string? inputString = null,
+            string pattern = @"^\d{4}(-\d{2})+(?=T)"
+        )
         {
-            if (inputDate.Equals(""))
+            // By default return date of first line of log file
+            if (inputString == null)
             {
                 // This method should consider different allowed day time
-                return Regex.Match(LogReader()[0], @"^\d{4}(-\d{2})+(?=T)").ToString();
+                return Regex.Match(LogReader()[0], pattern).ToString();
             }
+            // Return date of income string in NodaTime library format
             else
             {
-                return Regex.Match(inputDate, @"^\d{4}(-\d{2})+(?=T)").ToString();
+                return Regex.Match(inputString, pattern).ToString();
             }
         }
 
-        // (?<=T)\d{2}:\d{2}:\d{2}(?=\s) time regex
+        // Return time part of string
+        public static string Time(string? inputTime = null)
+        {
+            return Date(inputTime, @"(?<=T)\d{2}:\d{2}:\d{2}(?=\s)");
+        }
+        // (?<=T)\d{2}:\d{2}:\d{2}(?=\s) time regex 
     }
 }
