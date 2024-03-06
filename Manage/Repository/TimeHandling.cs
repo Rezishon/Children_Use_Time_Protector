6532b41f2bb07f436055915fda4e431037d4b474
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-03-06 23:48:32 +0330

    fix: add XML description

--- Manage/Repository/TimeHandling.cs
+++ Manage/Repository/TimeHandling.cs
@@ -2,6 +2,13 @@ namespace TimeHandling
 {
     public static class Time
     {
+        /// <summary>
+        /// User allowed to use or not.
+        /// </summary>
+        /// <returns>
+        ///     <term>Bool?</term>
+        ///     <description>True for allowed | False for not allowed | null for 10 minute remaining</description>
+        /// </returns>
         public static bool? Allowed()
         {
             int UsedDurationTime = 0;
