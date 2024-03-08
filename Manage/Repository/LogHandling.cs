45f2126725a62cd6dc62c881ca4aa0951e4dbc4b
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-03-08 23:59:14 +0330

    fix: add XML documentation

--- Manage/Repository/LogHandling.cs
+++ Manage/Repository/LogHandling.cs
@@ -5,6 +5,9 @@ namespace LogHandling
 {
     public class LogFile
     {
+        /// <summary>
+        /// It find application path and then custom the path string
+        /// </summary>
         public static string LogFilePath { get; } =
             Regex.Replace(
                 Commands.Pwd(),
@@ -12,17 +15,42 @@ namespace LogHandling
                 @"\Children_Use_Time_Protector\Manage\bin\Debug\net8.0\Log.cutp"
             );
 
+        /// <summary>
+        /// Read log file content
+        /// </summary>
+        /// <returns>
+        ///     <list type="bullet|number|table">
+        ///         <item>
+        ///             <term>Array of string</term>
+        ///             <description>Each index contains content of each line in log file</description>
+        ///         </item>
+        ///     </list>
+        /// </returns>
         public static string[] LogReader()
         {
             return File.ReadAllLines(LogFilePath);
         }
 
+        /// <summary>
+        /// Clear file content
+        /// </summary>
         public static void LogCleaner()
         {
             File.WriteAllText(LogFilePath, "");
         }
 
         public static string Date(
+        /// <summary>
+        /// Return date part of string<br/>
+        /// Optionally, this method can find date of income string or other patterns
+        /// </summary>
+        /// <param name="inputString">String which you want to find date from it</param>
+        /// <param name="pattern">Pattern to find things more than date, like time</param>
+        /// <returns>
+        ///     <term>String</term>
+        ///     <description>Found pattern | By default it's date string | Can be null</description>
+        /// </returns>
+        /// <remarks>This method is a multifunction method</remarks>
             string? inputString = null,
             string pattern = @"^\d{4}(-\d{2})+(?=T)"
         )
@@ -41,6 +69,16 @@ namespace LogHandling
         }
 
         public static string Time(string? inputTime = null)
+        /// <summary>
+        /// Return time part of string<br/>
+        /// Optionally, this method can find time of income string
+        /// </summary>
+        /// <param name="inputTime">String which you want to find time from it</param>
+        /// <returns>
+        ///     <term>String</term>
+        ///     <description>Found pattern | Can be null</description>
+        /// </returns>
+        /// <remarks>By default it find first log line time value</remarks>
         {
             return Date(inputTime, @"(?<=T)\d{2}:\d{2}:\d{2}(?=\s)");
         }
