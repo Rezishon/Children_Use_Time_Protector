a5bc63f0a3d8699a0d4d9b664efe4a97c9de39e4
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-03-08 23:58:38 +0330

    fix: remove old comments
    Old comments removed to replaced with new documentation methods

--- Manage/Repository/LogHandling.cs
+++ Manage/Repository/LogHandling.cs
@@ -5,7 +5,6 @@ namespace LogHandling
 {
     public class LogFile
     {
-        // Log file Path property
         public static string LogFilePath { get; } =
             Regex.Replace(
                 Commands.Pwd(),
@@ -13,19 +12,16 @@ namespace LogHandling
                 @"\Children_Use_Time_Protector\Manage\bin\Debug\net8.0\Log.cutp"
             );
 
-        // Read log file content
         public static string[] LogReader()
         {
             return File.ReadAllLines(LogFilePath);
         }
 
-        // Clear file content
         public static void LogCleaner()
         {
             File.WriteAllText(LogFilePath, "");
         }
 
-        // Return date part of string
         public static string Date(
             string? inputString = null,
             string pattern = @"^\d{4}(-\d{2})+(?=T)"
@@ -44,11 +40,9 @@ namespace LogHandling
             }
         }
 
-        // Return time part of string
         public static string Time(string? inputTime = null)
         {
             return Date(inputTime, @"(?<=T)\d{2}:\d{2}:\d{2}(?=\s)");
         }
-        // (?<=T)\d{2}:\d{2}:\d{2}(?=\s) time regex 
     }
 }
