71b4dca9658efe15a2eaf9663fac823e8e35b078
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-03-12 23:31:34 +0330

    feat: add XML documentation

--- Manage/Repository/Messages.cs
+++ Manage/Repository/Messages.cs
@@ -2,6 +2,9 @@ using System.Text.RegularExpressions;
 
 namespace Children_Use_Time_Protector.Repository
 {
+    /// <summary>
+    /// Include messages we want to show to user
+    /// </summary>
     public class Messages
     {
         public static string Say_App_Name()
