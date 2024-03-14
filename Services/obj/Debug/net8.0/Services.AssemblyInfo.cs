b52da5ee277102a1f72dfc5b39508a12bc9c9162
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-03-14 23:39:42 +0330

    chore: dotnet run

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba804cb9cbd5f084415ece38948e0630160760e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bae82d32040e242ab7bcb084864a7959c61fec01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
