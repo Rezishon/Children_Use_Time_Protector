a0dc04e446a690203c40dff19a633cdb33fc743a
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-03-17 13:43:05 +0330

    chore: remove comments
    This comment was a reminder

--- Manage/Repository/ConfigHandling.cs
+++ Manage/Repository/ConfigHandling.cs
@@ -393,8 +393,6 @@ namespace ConfigHandling
 
         public class SetConfigToRoot
         {
-            // All methods pass the line name root
-            // for each part name we should have a method separately which the lineName is root and the the partName would be the method name
         }
 
         public class SetConfigToService
