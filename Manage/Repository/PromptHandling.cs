b89a1fd360ce0c9af26902f0e21e41751c081d1a
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-03-30 00:00:14 +0330

    fix: comment old logic

--- Manage/Repository/PromptHandling.cs
+++ Manage/Repository/PromptHandling.cs
@@ -168,6 +168,16 @@ namespace Manage.Repository
                     )
                 );
 
+                // if (
+                //     (!IsTemp && usingTime % 10 != 0 || (usingTime < 10 && usingTime > 1430))
+                //     || (IsTemp && usingTime % 10 != 0 || (usingTime < 0 && usingTime > 1430))
+                // )
+                // {
+                //     ExitProcess(
+                //         $"[red][bold]{timeName}[/] is out of range. It must be a multiple of 10 & between [underline]{(IsTemp == true ? "0" : "10")}[/] and [underline]1430[/]\nPress any key to Repeat[/]"
+                //     );
+                //     continue;
+                // }
                 {
                 }
 
