c38d6224611f6de445ad971b685effff5ed2ffbf
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-03-31 23:36:12 +0330

    fix: replace codes
    Du to new condition new codes should be inside the condition which I did here

--- Manage/Program.cs
+++ Manage/Program.cs
@@ -195,10 +195,10 @@ class Program
 
                     #region Test
                     case 3:
-                        ConfigSetter.SetConfigToRoot.Status(Hash.ToSha256("0"));
-                        ConfigSetter.SetConfigToService.Status(Hash.ToSha256("0"));
                         if (Environment.GetEnvironmentVariable("Testing") == "true")
                         {
+                            ConfigSetter.SetConfigToRoot.Status(Hash.ToSha256("0"));
+                            ConfigSetter.SetConfigToService.Status(Hash.ToSha256("0"));
                         }
                         break;
                     #endregion
