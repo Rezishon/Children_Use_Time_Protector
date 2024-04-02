2e639c14ec95a1b174ffabd2629dcdfc10c7a9ae
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-04-02 22:01:13 +0330

    feat: add allowed class
    Du to "Clean code" book, I decided to add a class for allowed use time for better code. I will add a validation code.

--- Manage/Repository/PromptHandling.cs
+++ Manage/Repository/PromptHandling.cs
@@ -151,6 +151,7 @@ namespace Manage.Repository
             bool IsNew = true,
             bool IsTemp = false
         )
+        public class AllowedUseTime
         {
             bool flag = true;
 
