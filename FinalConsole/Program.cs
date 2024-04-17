9a9300c67e9f07e50349a2bbd2ed9a9d3c57b238
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-04-17 23:44:48 +0330

    feat: add notification sender
    This code will send notification to windows. Each line is same as NuGet package guide

--- FinalConsole/Program.cs
+++ FinalConsole/Program.cs
@@ -6,5 +6,12 @@ class Program
 {
     static void Main(string[] args)
     {
+       // Requires Microsoft.Toolkit.Uwp.Notifications NuGet package version 7.0 or greater
+            new ToastContentBuilder()
+                .AddArgument("action", "viewConversation")
+                .AddArgument("conversationId", 9813)
+                .AddText("Andrew sent you a picture")
+                .AddText("Check this out, The Enchantments in Washington!")
+                .Show();
     }
 }
