04df1e935e3c582a78a65406cddfad16252ca4b6
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-04-17 23:40:20 +0330

    chore: dotnet run & build

--- Manage/obj/Debug/net8.0/Children_Use_Time_Protector.AssemblyInfo.cs
+++ Manage/obj/Debug/net8.0/Children_Use_Time_Protector.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Children_Use_Time_Protector")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee21fda774f4f8e63164ac569df9daaf0c50fc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+521395acdd22303383673214e72097c088ab3a5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Children_Use_Time_Protector")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Children_Use_Time_Protector")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
