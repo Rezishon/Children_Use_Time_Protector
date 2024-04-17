71b52b532b3eacf601299899c0969d11642c9959
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-04-17 23:43:29 +0330

    chore: additional files

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+521395acdd22303383673214e72097c088ab3a5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c43e261f26b6738e72a9f2a8a84a59424d2247e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
